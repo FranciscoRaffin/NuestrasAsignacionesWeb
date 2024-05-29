e7f417c03462789dd73faf557682a021ac14e2e1
Francisco Raffin <142631559+FranciscoRaffin@users.noreply.github.com>
2024-05-29 14:45:56 -0300

    renunció?.js

--- locales_data.js
+++ locales_data.js
@@ -12,7 +12,7 @@ const bernal_colab = [
     [ "131018" ,   "Rocio Miranda"        ],
     [ "131021" ,   "Alejo Aguirre"        ],
     [ "B9830" ,    "Erica Rosso"          ],
-    [ "131059" ,   "Natalie Fernandez (Renunció)"    ],
+    [ "131059" ,   "Natalie Fernandez (''Renunció'')"    ],
     [ "131057" ,   "Florencia Ayala"      ],
     [ "95085" ,    "Agustin Cornejo"      ],
     [ "131005" ,   "Lautaro  Lezcano"     ],
