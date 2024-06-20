05443d27311d4b7fc633bb874e821bf29ec258a1
Francisco Raffin <142631559+FranciscoRaffin@users.noreply.github.com>
2024-06-20 17:55:15 -0300

    Update locales_data.js

--- locales_data.js
+++ locales_data.js
@@ -1,43 +1,31 @@
 
-
 const bernal_colab = [
 //  |Legajo  [0]   |Nombre [1]
- // [ "131054" ,   "Francisco Raffin"     ],
-    [ "131043" ,   "Samuel De Los Santos"],
- // [ "131065" ,   "Milagros Birtos"      ],
-    [ "131045" ,   "Rochio Marino"        ],
-    [ "131046" ,   "Lautaro Perez"        ],
-    [ "131047" ,   "Facundo Barta"        ],
-    [ "131053" ,   "Franco Delgado"       ],
-    [ "131018" ,   "Rocio Miranda"        ],
-    [ "131021" ,   "Alejo Aguirre"        ],
-    [ "B9830" ,    "Erica Rosso"          ],
-    [ "131061" ,   "Luna Vasquez"         ],
-    [ "131057" ,   "Florencia Ayala"      ],
-    [ "95085" ,    "Agustin Cornejo"      ],
-    [ "131005" ,   "Lautaro  Lezcano"     ],
-    [ "61379" ,    "Agustina Amodeo"      ],
-    [ "131062" ,   "Natanael Gomez 👑"       ],
-    [ "131067" ,   "Danilo Torres"        ],
-    [ "131066" ,   "Fray Alucena"         ],
-    [ "131055" ,   "Luna Zugasti"         ],
-    [ "B6654"  ,   "Yamila Bizarap"       ],
-    [ "81164"  ,   "Juan Pablo"           ],
-    [ "95140"  ,   "Milena"               ]
-
+    [ "146031" ,   "Camila Belen Gonzales"     ],
+    [ "146012" ,   "Maria Luz Echavarria"      ],
+    [ "146028" ,   "Milton Leonardo Suarez"    ],
+    [ "146025" ,   "Aneley Milene Sanchez"     ],
+    [ "146024" ,   "Gonzalo Fabian Retamoso"   ],
+    [ "146020" ,   "Sol Leonella Martinez"     ],
+    [ "146027" ,   "Yadira Ileana Singer"      ],
+    [ "146011" ,   "Camila Adriana Diaz"       ],
+    [ "146009" ,   "Lourdes Jeanette Cejas"    ],
+    [ "146030" ,   "Valentina Raquel Oggier"   ],
+    [ "146002" ,   "Nicolas Miguel Alonso"     ],
+    [ "146004" ,   "Agustin Isaias Arriondo"   ],
+    [ "146026" ,   "Lautaro Dylan Silvan"      ],
+    [ "146001" ,   "Victoria Rita Aguirre"     ],
+    [ "146003" ,   "Ismael Hernan Aquino"      ],
+    [ "146007" ,   "Santiago Martin Cabral "   ],
+    [ "146008" ,   "Sasha Denise Carabajal"    ],
+    [ "146010" ,   "Maria del Rosa Cruz Joya"  ],
+    [ "146014" ,   "Mario Alberto Franco"      ],
+    [ "146016" ,   "Yamila Stefania Godoy"     ],
+    [ "146018" ,   "Luciana Victoria Gonzalez" ],
+    [ "146019" ,   "Lautaro Nicolas Jaume"     ],
+    [ "146021" ,   "Romanela Betsab Mendez"    ],
+    [ "146022" ,   "Melina Ailen Mendoza"      ],
+    [ "146023" ,   "Lousana Lorele Retamoso"   ],
+    [ "146029" ,   "Bautista Mateo Valdez"     ]
 
 ];
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
