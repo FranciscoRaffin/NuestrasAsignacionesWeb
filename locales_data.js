2af7c2592243d6277e9ef7588740a833bd5e0668
Francisco Raffin <142631559+FranciscoRaffin@users.noreply.github.com>
2024-07-28 14:36:56 -0300

    Update locales_data.js

--- locales_data.js
+++ locales_data.js
@@ -3,7 +3,7 @@
 const bernal_colab = [
 //  |Legajo  [0]   |Nombre [1]
  // [ "131054" ,   "Francisco Raffin"     ],
-    [ "131043" ,   "Samuel De Los Santos"],
+    [ "131043" ,   "Samuel De Los Santos" ],
     [ "131065" ,   "Milagros Birtos"      ],
     [ "131045" ,   "Rochio Marino 👑"     ],
     [ "131046" ,   "Lautaro Perez"        ],
